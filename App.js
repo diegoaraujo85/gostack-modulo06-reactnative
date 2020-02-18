3469f07c38c79776270d62fa1930c218feea50f9
diegoaraujo85 <doasalah@gmail.com>
2020-02-18 10:31:57 -0300

    projeto inicial

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Oi Diego</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default App;
