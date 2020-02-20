b410119c8ad6e93c483283daa0534ce34f2fc95a
diegoaraujo85 <doasalah@gmail.com>
2020-02-20 02:22:20 -0300

    criando a navegaçao e rotas

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import 'react-native-gesture-handler';
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-console.tron.warn('hi');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Diego!</Text>
-      </View>
-    </>
-  );
+  return <Routes />;
 }
